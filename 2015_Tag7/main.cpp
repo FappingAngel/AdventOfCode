827ec37d76d9a4d8a7f86f56e3cbe8f16cdfb2b3
NonToxic <cop2002@gmx.de>
2022-03-17 09:21:48 +0100

    Einige Fehler behoben, übersichtlicher gemacht, in Funktionen ausgelagert und gekürzt, jedoch noch Fehlerhaft (Zahlen werden nicht richtig verarbeitet)

--- 2015_Tag7/main.cpp
+++ 2015_Tag7/main.cpp
@@ -6,9 +6,21 @@
 
 using namespace std;
 
-/*short LShift(short a, int b){
-    return (short)(a<<b);
-} */
+map<string, unsigned short> Namen;
+
+bool inputCheck(string);
+
+void NOT(string);
+
+void IS(string);
+
+void AND(string);
+
+void OR(string);
+
+void RSHIFT(string);
+
+void LSHIFT(string);
 
 int main() {
     ifstream file("C:\\Users\\Max\\CLionProjects\\AdventOfCode\\2015_Tag7\\input.txt");
@@ -18,141 +30,162 @@ int main() {
     }
 
     string zeile;
-    map<string, unsigned short> Namen;
-    //map <string, short> Namen;
     while (getline(file, zeile)) {
-        string substring;
-        string variable1, variable2, variable3;
-        short anzahl;
 
         // Modus finden
         if (zeile.find("NOT", 0) != string::npos) {
-
-            //String aufbrechen
-            substring = zeile.substr(4);
-            variable1 = substring.substr(0, substring.find(' '));
-            substring = substring.substr(substring.find(' ') + 4);
-            variable2 = substring.substr(0, substring.find('\r'));
-
-            //Überprüfen ob es Zahlen oder Strings sind, und dementsprechend rechnen
-            if (isdigit(variable1[0])) {
-                Namen[variable2] = (~stoi(variable1));
-            } else {
-                Namen.insert({variable1,0});
-                Namen[variable2] = (~Namen[variable1]);
-            }
-
+            NOT(zeile);
         } else if (zeile.find("AND", 0) != string::npos) {
-
-            variable1 = zeile.substr(0, zeile.find(' '));
-            substring = zeile.substr(zeile.find(' ') + 5);
-            variable2 = substring.substr(0, substring.find(' '));
-            substring = substring.substr(substring.find(' ') + 4);
-            variable3 = substring.substr(0, substring.find('\r'));
-
-            if (isdigit(variable1[0])) {
-                Namen.insert({variable2,0});
-                Namen[variable3] = (stoi(variable1) & Namen[variable2]);
-            } else if (isdigit(variable2[0])) {
-                Namen.insert({variable1,0});
-                Namen[variable3] = (Namen[variable1] & stoi(variable2));
-            } else {
-                Namen.insert({variable1,0});
-                Namen.insert({variable2,0});
-                Namen[variable3] = (Namen[variable1] & Namen[variable2]);
-            }
-
+            AND(zeile);
         } else if (zeile.find("OR", 0) != string::npos) {
-
-            variable1 = zeile.substr(0, zeile.find(' '));
-            substring = zeile.substr(zeile.find(' ') + 4);
-            variable2 = substring.substr(0, substring.find(' '));
-            substring = substring.substr(substring.find(' ') + 4);
-            variable3 = substring.substr(0, substring.find('\r'));
-
-            if (isdigit(variable1[0])) {
-                Namen.insert({variable2,0});
-                Namen[variable3] = (stoi(variable1) | Namen[variable2]);
-            } else if (isdigit(variable2[0])) {
-                Namen.insert({variable1,0});
-                Namen[variable3] = (Namen[variable1] | stoi(variable2));
-            } else {
-                Namen.insert({variable1,0});
-                Namen.insert({variable2,0});
-                Namen[variable3] = (Namen[variable1] | Namen[variable2]);
-            }
-
+            OR(zeile);
         } else if (zeile.find("RSHIFT", 0) != string::npos) {
-
-            variable1 = zeile.substr(0, zeile.find(' '));
-            substring = zeile.substr(zeile.find(' ') + 8);
-            anzahl = stoi(substring.substr(0, substring.find(' ')));
-            substring = substring.substr(substring.find(' ') + 4);
-            variable3 = substring.substr(0, substring.find('\r'));
-
-            if (isdigit(variable1[0])) {
-                Namen[variable3] = (stoi(variable1) >> anzahl);
-            } else {
-                Namen.insert({variable1,0});
-                Namen[variable3] = (Namen[variable1] >> anzahl);
-            }
-
+            RSHIFT(zeile);
         } else if (zeile.find("LSHIFT", 0) != string::npos) {
-
-            variable1 = zeile.substr(0, zeile.find(' '));
-            substring = zeile.substr(zeile.find(' ') + 8);
-            anzahl = stoi(substring.substr(0, substring.find(' ')));
-            substring = substring.substr(substring.find(' ') + 4);
-            variable3 = substring.substr(0, substring.find('\r'));
-
-            if (isdigit(variable1[0])) {
-                Namen[variable3] = (stoi(variable1)) << anzahl;
-            } else {
-                Namen.insert({variable1,0});
-                Namen[variable3] = (Namen[variable1]) << anzahl;
-            }
-
+            LSHIFT(zeile);
         } else {
-
-            variable1 = zeile.substr(0, zeile.find(' '));
-            substring = zeile.substr(zeile.find(' ') + 4);
-            variable2 = substring.substr(0, substring.find('\r'));
-
-            if (isdigit(variable1[0])) {
-
-                Namen[variable2] = (stoi(variable1));
-
-            } else {
-
-                Namen.insert({variable1,0});
-                Namen[variable2] = Namen[variable1];
-
-            }
-
+            IS(zeile);
         }
+    }
+    for (map<string, unsigned short>::iterator itr = Namen.begin(); itr != Namen.end(); itr++) {
+        cout << "Name: " << itr->first
+             << " Wert: " << itr->second << endl;
+    }
+}
 
+bool inputCheck(string name) {
+    if (Namen.find(name) == Namen.end() && !isdigit(name[0]))
+        return false;
+    else return true;
+}
+
+void NOT(string zeile) {
+    string substring, variable1, variable3;
+
+    substring = zeile.substr(4);
+    variable1 = substring.substr(0, substring.find(' '));
+    substring = substring.substr(substring.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+
+    if (!inputCheck(variable1)) {
+        return;
     }
 
-     for (map<string, unsigned short>::iterator itr = Namen.begin(); itr != Namen.end(); itr++) {
-        cout <<"Name: "<< itr->first
-       <<" Wert: "<< itr->second << endl;
+    //Überprüfen ob es Zahlen oder Strings sind, und dementsprechend rechnen
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (~stoi(variable1));
+    } else {
+        Namen[variable3] = (~Namen[variable1]);
     }
 
 }
 
+void AND(string zeile) {
+    string substring, variable1, variable2, variable3;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 5);
+    variable2 = substring.substr(0, substring.find(' '));
+    substring = substring.substr(substring.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1) || !inputCheck(variable2)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1) & Namen[variable2]);
+    } else if (isdigit(variable2[0])) {
+        Namen[variable3] = (Namen[variable1] & stoi(variable2));
+    } else {
+        Namen[variable3] = (Namen[variable1] & Namen[variable2]);
+    }
+}
+
+void OR(string zeile) {
+    string substring, variable1, variable2, variable3;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 4);
+    variable2 = substring.substr(0, substring.find(' '));
+    substring = substring.substr(substring.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1) || !inputCheck(variable2)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1) | Namen[variable2]);
+    } else if (isdigit(variable2[0])) {
+        Namen[variable3] = (Namen[variable1] | stoi(variable2));
+    } else {
+        Namen[variable3] = (Namen[variable1] | Namen[variable2]);
+    }
+}
+
+void RSHIFT(string zeile) {
+    string substring, variable1, variable3;
+    short anzahl;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 8);
+    anzahl = stoi(substring.substr(0, substring.find(' ')));
+    substring = substring.substr(substring.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1) >> anzahl);
+    } else {
+        Namen[variable3] = (Namen[variable1] >> anzahl);
+    }
+}
+
+void LSHIFT(string zeile) {
+    string substring, variable1, variable3;
+    short anzahl;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 8);
+    anzahl = stoi(substring.substr(0, substring.find(' ')));
+    substring = substring.substr(substring.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1)) << anzahl;
+    } else {
+        Namen[variable3] = (Namen[variable1]) << anzahl;
+    }
+}
+
+void IS(string zeile) {
+    string substring, variable1, variable3;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1));
+
+    } else {
+        Namen[variable3] = Namen[variable1];
+    }
+}
+
 // Was ist a?
 
-// werte werden überschrieben -->
-
-
-//if(Namen.find(variable1) == Namen.end()){
-// nicht gefunden
-// }else{
-// gefunden
-// }
-
-
-// Auseinander bröseln
-// pair<>({});???
-// Bitweise short oder int?
-// Was wenn beide Variablen sind (and/Or)?
+// Was wenn Input eine Zahl?
