5391f53f20c2989b17ab849c7e9f17e03d9d96cc
NonToxic <cop2002@gmx.de>
2022-03-18 14:20:36 +0100

    Befehl hinzufegüft, um IfSstream zurückzusetzen, Teil 1/2 funktioniert

--- 2015_Tag7/main.cpp
+++ 2015_Tag7/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 map<string, unsigned short> Namen;
 
+//Funktionen deklarieren
+
 bool inputCheck(string);
 
 void NOT(string);
@@ -23,34 +25,51 @@ void RSHIFT(string);
 void LSHIFT(string);
 
 int main() {
+
+    //Inputdatei öffnen
     ifstream file("C:\\Users\\Max\\CLionProjects\\AdventOfCode\\2015_Tag7\\input.txt");
     if (!file.is_open()) {
         cerr << "Fehler: Datei konnte nicht geöffnet werden" << endl;
         return 0;
     }
-
+    int anzahl = 0, iteration;
+    bool kondition = true;
     string zeile;
-    while (getline(file, zeile)) {
+    while (kondition) {
+        iteration = 0;
+        anzahl ++;
+        while (getline(file, zeile)) {
 
-        // Modus finden
-        if (zeile.find("NOT", 0) != string::npos) {
-            NOT(zeile);
-        } else if (zeile.find("AND", 0) != string::npos) {
-            AND(zeile);
-        } else if (zeile.find("OR", 0) != string::npos) {
-            OR(zeile);
-        } else if (zeile.find("RSHIFT", 0) != string::npos) {
-            RSHIFT(zeile);
-        } else if (zeile.find("LSHIFT", 0) != string::npos) {
-            LSHIFT(zeile);
-        } else {
-            IS(zeile);
+            // Modus finden
+            if (zeile.find("NOT", 0) != string::npos) {
+                NOT(zeile);
+            } else if (zeile.find("AND", 0) != string::npos) {
+                AND(zeile);
+            } else if (zeile.find("OR", 0) != string::npos) {
+                OR(zeile);
+            } else if (zeile.find("RSHIFT", 0) != string::npos) {
+                RSHIFT(zeile);
+            } else if (zeile.find("LSHIFT", 0) != string::npos) {
+                LSHIFT(zeile);
+            } else {
+                IS(zeile);
+            }
+            iteration ++;
         }
+        if(inputCheck("a")){
+           kondition = false;
+        }
+        file.clear();
+        file.seekg(ios::beg);
+        cout << "Anzahl:Iterationen =  " << anzahl << " : " << iteration<< endl;
     }
+    //Ausgabe der Map
     for (map<string, unsigned short>::iterator itr = Namen.begin(); itr != Namen.end(); itr++) {
         cout << "Name: " << itr->first
              << " Wert: " << itr->second << endl;
     }
+    file.close();
+    return 0;
 }
 
 bool inputCheck(string name) {
@@ -62,12 +81,13 @@ bool inputCheck(string name) {
 void NOT(string zeile) {
     string substring, variable1, variable3;
 
+    //Zeile aufteilen
     substring = zeile.substr(4);
     variable1 = substring.substr(0, substring.find(' '));
     substring = substring.substr(substring.find(' ') + 4);
     variable3 = substring.substr(0, substring.find('\r'));
 
-
+    //Inputs valide?
     if (!inputCheck(variable1)) {
         return;
     }
@@ -79,6 +99,25 @@ void NOT(string zeile) {
         Namen[variable3] = (~Namen[variable1]);
     }
 
+}
+
+void IS(string zeile) {
+    string substring, variable1, variable3;
+
+    variable1 = zeile.substr(0, zeile.find(' '));
+    substring = zeile.substr(zeile.find(' ') + 4);
+    variable3 = substring.substr(0, substring.find('\r'));
+
+    if (!inputCheck(variable1)) {
+        return;
+    }
+
+    if (isdigit(variable1[0])) {
+        Namen[variable3] = (stoi(variable1));
+
+    } else {
+        Namen[variable3] = Namen[variable1];
+    }
 }
 
 void AND(string zeile) {
@@ -167,25 +206,7 @@ void LSHIFT(string zeile) {
     }
 }
 
-void IS(string zeile) {
-    string substring, variable1, variable3;
+//weiter machen, bis Zeile 7 wahr ist
+//ifstream liest nicht neu aus?
+//file.close() nach vorne verschieben
 
-    variable1 = zeile.substr(0, zeile.find(' '));
-    substring = zeile.substr(zeile.find(' ') + 4);
-    variable3 = substring.substr(0, substring.find('\r'));
-
-    if (!inputCheck(variable1)) {
-        return;
-    }
-
-    if (isdigit(variable1[0])) {
-        Namen[variable3] = (stoi(variable1));
-
-    } else {
-        Namen[variable3] = Namen[variable1];
-    }
-}
-
-// Was ist a?
-
-// Was wenn Input eine Zahl?
